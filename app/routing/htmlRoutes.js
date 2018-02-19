341220dab8742737db6176ed804d3da35042fbde
zhagan <zhagan@gmail.com>
2018-02-18 20:52:10 -0700

    matchmaker port

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,11 @@ var path = require("path");
 module.exports = function(app) {
   app.get("/:word?", function(req, res) {
     var word = req.params.word;
-    
+
     if (word === "survey") {
       res.sendFile(path.join(__dirname, "/../public/survey.html"));
-    } 
-    
+    }
+
     else {
       res.sendFile(path.join(__dirname, "/../public/home.html"));
     }
